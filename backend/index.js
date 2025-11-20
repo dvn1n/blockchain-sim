7da7efeed831a380f836733bf1d3009de6115edf
dvn1n <sciencekdi09@naver.com>
2025-11-20 16:22:36 +0900

    backend fr

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,15 @@
 const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 3000;
 const cors = require('cors');
-const PORT = process.env.port || 3000;
-
 const Block = require('./Block');
 const Blockchain = require('./Blockchain');
-
 const app = express();
-app.use(express.json());
-app.use(cors());
-
 const chain = new Blockchain();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('/chain', (req, res) => {
     res.json(chain.getChain());
@@ -19,6 +19,10 @@ app.post('/add', (req, res) => {
     const {data} = req.body;
     chain.createBlock(new Block(chain.chain.length, Date.now(), data));
     res.json(chain.getChain());
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 app.listen(PORT, () => console.log(`backend on ${PORT}`));
