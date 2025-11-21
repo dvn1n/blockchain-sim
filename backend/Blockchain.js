b825cb5b4e995dfe47bc45b99e6d564fe96275e7
dvn1n <sciencekdi09@naver.com>
2025-11-21 12:00:53 +0900

    fucking miner

--- backend/Blockchain.js
+++ backend/Blockchain.js
@@ -3,7 +3,7 @@ const Block = require('./Block');
 class Blockchain {
     constructor() {
         this.chain = [new Block(0, Date.now(), "Genesis Block", "0")];
-        this.diff = 3;
+        this.diff = 1;
     }
 
     getLastestBlock() {
